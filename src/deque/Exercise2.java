8b3097ae24d1c17de133d3998e41d913de5c6b9a
iseungsoo07 <iseungsoo07@gmail.com>
2023-07-20 15:30:26 +0900

    데크 연습문제

---
+++ src/deque/Exercise2.java
@@ -0,0 +1,54 @@
+package deque;
+
+// Exercise2
+// Palindrome 찾기
+// Palindrome 이면 true, 아니면 false 를 반환하세요.
+// Palindrome: 앞으로 읽어도 거꾸로 읽어도 같은 문자열
+
+// 입출력 예시)
+// 입력: a
+// 결과: true
+
+// 입력: madam
+// 결과: true
+
+// 입력: abab
+// 결과: false
+
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Exercise2 {
+    public static boolean checkPalindrome(String str) {
+        Deque<String> deque = new ArrayDeque<>();
+        boolean isPalindrome = true;
+
+        for (String s : str.split("")) {
+            deque.addLast(s);
+        }
+
+        while (!deque.isEmpty()) {
+            String s1 = deque.pollFirst(); // remove를 쓰면 예외가 발생하기 때문에 null을 반환하는 poll을 사용한다.
+            String s2 = deque.pollLast();
+
+            if (s1 != null && s2 != null && !s1.equals(s2)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        return isPalindrome;
+    }
+
+    public static void main(String[] args) {
+        // Test code
+        System.out.println(checkPalindrome("a"));       // true
+        System.out.println(checkPalindrome("aba"));     // true
+        System.out.println(checkPalindrome("abba"));    // true
+        System.out.println(checkPalindrome("abab"));    // false
+        System.out.println(checkPalindrome("abcba"));   // true
+        System.out.println(checkPalindrome("abccba"));  // true
+        System.out.println(checkPalindrome("madam"));  // true
+    }
+}
--- src/stack/Main.java
+++ src/stack/Main.java
@@ -6,7 +6,7 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack();
+        Stack<Integer> stack = new Stack<>();
 
         stack.push(1);
         stack.push(2);
